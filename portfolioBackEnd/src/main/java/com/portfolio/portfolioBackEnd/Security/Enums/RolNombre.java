6906e863742e8e443ba97196cf9e95a002278f64
Danilo Bascourleguy <danilobasco4@hotmail.com>
2023-05-14 23:36:31 -0300

    Cambio de directorio

--- portfolioBackEnd/src/main/java/com/portfolio/portfolioBackEnd/Security/Enums/RolNombre.java
+++
@@ -1,8 +0,0 @@
-
-package com.portfolio.portfolioBackEnd.Security.Enums;
-
-
-public enum RolNombre {
-ROLE_ADMIN, ROLE_USER
-    
-}
---
+++ src/main/java/com/portfolio/portfolioBackEnd/Repository/IPersonaRepository.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.portfolioBackEnd.Repository;
+
+import com.portfolio.portfolioBackEnd.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
