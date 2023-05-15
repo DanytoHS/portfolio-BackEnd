be39e2951731fabc260e66dda35d0ba3579b60e8
Danilo Bascourleguy <danilobasco4@hotmail.com>
2023-05-15 13:40:09 -0300

    CORS3

--- src/main/java/com/portfolio/portfolioBackEnd/Controller/PersonaController.java
+++ src/main/java/com/portfolio/portfolioBackEnd/Controller/PersonaController.java
@@ -6,7 +6,6 @@ import com.portfolio.portfolioBackEnd.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-danilo-bascourleguy.web.app")
 public class PersonaController {
      @Autowired IPersonaService ipersonaService ;
 
